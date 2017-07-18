5680a05fe4fab61a6640f83cc22aa576004d9f76
Arturo Filastò <arturo@filasto.net>
2017-07-18 18:49:41 +0200

    Fixup 1->2 db migration script

--- proteus-events/data/migrations/2_add_jobs_state.sql
+++ proteus-events/data/migrations/2_add_jobs_state.sql
@@ -6,6 +6,20 @@ ALTER TABLE jobs DROP COLUMN IF EXISTS state;
 
 -- +migrate Up
 -- +migrate StatementBegin
-CREATE TYPE JOB_STATE AS ENUM ('active', 'deleted', 'done');
-ALTER TABLE jobs ADD COLUMN state JOB_STATE;
+DO $$
+    BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'job_state') THEN
+    CREATE TYPE JOB_STATE AS ENUM ('active', 'deleted', 'done');
+    END IF;
+END$$;
+
+DO $$
+    BEGIN
+        BEGIN
+            ALTER TABLE jobs ADD COLUMN state JOB_STATE;
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column `state` already exists in `jobs`.';
+        END;
+    END;
+$$
 -- +migrate StatementEnd
