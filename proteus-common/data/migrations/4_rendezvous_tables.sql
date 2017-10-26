3f487c79b5453b275b12954a17b3d91ca9439805
Joe Landers <joe@joelanders.net>
2017-10-26 13:02:34 -0400

    more sketching around migration

--- proteus-common/data/migrations/4_rendezvous_tables.sql
+++ proteus-common/data/migrations/4_rendezvous_tables.sql
@@ -4,6 +4,14 @@
 DROP TABLE IF EXISTS collectors;
 DROP TABLE IF EXISTS urls;
 DROP TABLE IF EXISTS test_helpers;
+DROP TABLE IF EXISTS url_categories;
+
+DROP SEQUENCE IF EXISTS cat_no_seq;
+DROP SEQUENCE IF EXISTS url_no_seq;
+DROP SEQUENCE IF EXISTS country_no_seq;
+DROP SEQUENCE IF EXISTS collector_no_seq;
+
+DROP TYPE IF EXISTS COLLECTOR_TYPE;
 
 -- +migrate StatementEnd
 
@@ -22,7 +30,8 @@ CREATE TABLE IF NOT EXISTS collectors
 (
   collector_no INTEGER DEFAULT nextval('collector_no_seq'::regclass) PRIMARY KEY NOT NULL,
   type COLLECTOR_TYPE,
-  address VARCHAR
+  address VARCHAR,
+  front_domain VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS urls
@@ -35,7 +44,7 @@ CREATE TABLE IF NOT EXISTS test_helpers
 
 );
 
-CREATE TABLE IF NOT EXISTS test_helpers
+CREATE TABLE IF NOT EXISTS url_categories
 (
 
 );
