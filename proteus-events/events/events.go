c3701e3e1b88de4d931f816d22afe936fc8475cb
Arturo Filastò <arturo@filasto.net>
2017-12-07 17:28:06 +0200

    Refactor the constants into their own file and use proper naming

--- proteus-events/events/events.go
+++ proteus-events/events/events.go
@@ -82,57 +82,6 @@ type JobData struct {
 	CreationTime time.Time `json:"creation_time"`
 }
 
-var all_cat_codes = map[string]struct{}{"ALDR": {}, "REL": {}, "PORN": {},
-	"PROV": {}, "POLR": {}, "HUMR": {}, "ENV": {}, "MILX": {},
-	"HATE": {}, "NEWS": {}, "XED": {}, "PUBH": {}, "GMB": {},
-	"ANON": {}, "DATE": {}, "GRP": {}, "LGBT": {}, "FILE": {},
-	"HACK": {}, "COMT": {}, "MMED": {}, "HOST": {}, "SRCH": {},
-	"GAME": {}, "CULTR": {}, "ECON": {}, "GOVT": {}, "COMM": {},
-	"CTRL": {}, "IGO": {}, "MISC": {}}
-
-var all_country_codes = map[string]struct{}{
-	"AD": {}, "AE": {}, "AF": {}, "AG": {}, "AI": {}, "AL": {},
-	"AM": {}, "AN": {}, "AO": {}, "AQ": {}, "AR": {}, "AS": {},
-	"AT": {}, "AU": {}, "AW": {}, "AZ": {}, "BA": {}, "BB": {},
-	"BD": {}, "BE": {}, "BF": {}, "BG": {}, "BH": {}, "BI": {},
-	"BJ": {}, "BM": {}, "BN": {}, "BO": {}, "BR": {}, "BS": {},
-	"BT": {}, "BU": {}, "BV": {}, "BW": {}, "BY": {}, "BZ": {},
-	"CA": {}, "CC": {}, "CF": {}, "CG": {}, "CH": {}, "CI": {},
-	"CK": {}, "CL": {}, "CM": {}, "CN": {}, "CO": {}, "CR": {},
-	"CS": {}, "CU": {}, "CV": {}, "CX": {}, "CY": {}, "CZ": {},
-	"DD": {}, "DE": {}, "DJ": {}, "DK": {}, "DM": {}, "DO": {},
-	"DZ": {}, "EC": {}, "EE": {}, "EG": {}, "EH": {}, "ER": {},
-	"ES": {}, "ET": {}, "FI": {}, "FJ": {}, "FK": {}, "FM": {},
-	"FO": {}, "FR": {}, "FX": {}, "GA": {}, "GB": {}, "GD": {},
-	"GE": {}, "GF": {}, "GH": {}, "GI": {}, "GL": {}, "GM": {},
-	"GN": {}, "GP": {}, "GQ": {}, "GR": {}, "GS": {}, "GT": {},
-	"GU": {}, "GW": {}, "GY": {}, "HK": {}, "HM": {}, "HN": {},
-	"HR": {}, "HT": {}, "HU": {}, "ID": {}, "IE": {}, "IL": {},
-	"IN": {}, "IO": {}, "IQ": {}, "IR": {}, "IS": {}, "IT": {},
-	"JM": {}, "JO": {}, "JP": {}, "KE": {}, "KG": {}, "KH": {},
-	"KI": {}, "KM": {}, "KN": {}, "KP": {}, "KR": {}, "KW": {},
-	"KY": {}, "KZ": {}, "LA": {}, "LB": {}, "LC": {}, "LI": {},
-	"LK": {}, "LR": {}, "LS": {}, "LT": {}, "LU": {}, "LV": {},
-	"LY": {}, "MA": {}, "MC": {}, "MD": {}, "MG": {}, "MH": {},
-	"ML": {}, "MN": {}, "MM": {}, "MO": {}, "MP": {}, "MQ": {},
-	"MR": {}, "MS": {}, "MT": {}, "MU": {}, "MV": {}, "MW": {},
-	"MX": {}, "MY": {}, "MZ": {}, "NA": {}, "NC": {}, "NE": {},
-	"NF": {}, "NG": {}, "NI": {}, "NL": {}, "NO": {}, "NP": {},
-	"NR": {}, "NT": {}, "NU": {}, "NZ": {}, "OM": {}, "PA": {},
-	"PE": {}, "PF": {}, "PG": {}, "PH": {}, "PK": {}, "PL": {},
-	"PM": {}, "PN": {}, "PR": {}, "PT": {}, "PW": {}, "PY": {},
-	"QA": {}, "RE": {}, "RO": {}, "RU": {}, "RW": {}, "SA": {},
-	"SB": {}, "SC": {}, "SD": {}, "SE": {}, "SG": {}, "SH": {},
-	"SI": {}, "SJ": {}, "SK": {}, "SL": {}, "SM": {}, "SN": {},
-	"SO": {}, "SR": {}, "ST": {}, "SU": {}, "SV": {}, "SY": {},
-	"SZ": {}, "TC": {}, "TD": {}, "TF": {}, "TG": {}, "TH": {},
-	"TJ": {}, "TK": {}, "TM": {}, "TN": {}, "TO": {}, "TP": {},
-	"TR": {}, "TT": {}, "TV": {}, "TW": {}, "TZ": {}, "UA": {},
-	"UG": {}, "UM": {}, "US": {}, "UY": {}, "UZ": {}, "VA": {},
-	"VC": {}, "VE": {}, "VG": {}, "VI": {}, "VN": {}, "VU": {},
-	"WF": {}, "WS": {}, "YD": {}, "YE": {}, "YT": {}, "YU": {},
-	"ZA": {}, "ZM": {}, "ZR": {}, "ZW": {}, "XX": {}}
-
 // AddJob adds a job to the database and run it
 func AddJob(db *sqlx.DB, jd JobData, s *Scheduler) (string, error) {
 	var (
@@ -846,7 +795,7 @@ func Start() {
 			} else {
 				countries = []string{"XX", probeCc}
 			}
-			countries_upper, err := UpperAndWhitelist(countries, all_country_codes)
+			countries_upper, err := UpperAndWhitelist(countries, AllCountryCodes)
 			if err != nil {
 				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 				return
@@ -856,7 +805,7 @@ func Start() {
 			if catParam != "" {
 				cats = strings.Split(c.Query("cat_code"), ",")
 			}
-			cats_upper, err := UpperAndWhitelist(cats, all_cat_codes)
+			cats_upper, err := UpperAndWhitelist(cats, AllCatCodes)
 			if err != nil {
 				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 				return
