a470308a25e590fbad8be239d1a5b689186c2ad1
Arturo Filastò <arturo@filasto.net>
2018-04-19 19:49:48 +0200

    Update base building-blocks

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -1,67 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
-import theme from '../static/theme'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
-import Drawer from 'react-toolbox/lib/drawer/Drawer'
-import MenuItem from 'react-toolbox/lib/menu/MenuItem'
-import AppBar from 'react-toolbox/lib/app_bar/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Button from 'material-ui/Button'
 
 import Link from 'next/link'
-
-import Meta from './meta'
+import Head from 'next/head'
 
 import pkgJson from '../package.json'
 
+Router.onRouteChangeStart = (url) => {
+  console.log("Loading ", url)
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
+
 export default class extends React.Component {
-
   static propTypes = {
-    children: React.PropTypes.node.isRequired,
-    title: React.PropTypes.string
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      drawerOpen: false
-    }
-    this.toggleDrawer = this.toggleDrawer.bind(this)
-  }
-
-  toggleDrawer () {
-    this.setState({
-      drawerOpen: !this.state.drawerOpen
-    })
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string
   }
 
   render () {
-    const {
-      drawerOpen
-    } = this.state
     let {
       title,
       children
     } = this.props
     if (!title) {
-      title = "Proteus"
+      title = "Orchestra"
     }
     return (
-      <ThemeProvider theme={theme}>
       <div>
-        <Meta />
+        <Head>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
         <header>
-          <Drawer
-            onOverlayClick={() => this.setState({drawerOpen: false})}
-            active={drawerOpen}>
-            <Link href='/'><MenuItem icon='home'>Home</MenuItem></Link>
-            <Link href='/admin/jobs'><MenuItem icon='event'>Jobs</MenuItem></Link>
-            <Link href='/admin/clients'><MenuItem icon='device_hub'>Clients</MenuItem></Link>
-          </Drawer>
-          <AppBar
-            title={title}
-            leftIcon='menu'
-            onLeftIconClick={() => (this.toggleDrawer())}
-          />
+          <Toolbar>
+          <Link href='/'><Button>Home</Button></Link>
+          <Link href='/admin/jobs'><Button>Jobs</Button></Link>
+          <Link href='/admin/clients'><Button>Clients</Button></Link>
+          </Toolbar>
         </header>
         <div className='content'>
           { children }
@@ -71,32 +55,7 @@ export default class extends React.Component {
           OONI Proteus {pkgJson['version']}
           </div>
         </footer>
-        <style jsx>{`
-          header {
-            width: 100%;
-            margin-bottom: 20px;
-            padding: 0;
-          }
-          .content {
-            font-size: 14px;
-            color: #1c1c1c;
-          }
-          footer {
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
-            margin-top: 20px;
-            padding-top: 20px;
-            padding-bottom: 20px;
-          }
-          .footer-content {
-            padding: 48px 32px;
-            width: 100%;
-          }
-        `}
-        </style>
       </div>
-      </ThemeProvider>
     )
   }
 }
