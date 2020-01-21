901ed50cd3d1472281952b31211bb14b84273266
Simone Basso <bassosimone@gmail.com>
2020-01-21 13:28:40 +0100

    Run make fmt

--- orchestrate/cmd/start.go
+++ orchestrate/cmd/start.go
@@ -10,7 +10,7 @@ import (
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start the server",
-	Long: `This command starts the OONI orchestrate service.`,
+	Long:  `This command starts the OONI orchestrate service.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		orchestrate.Start()
 	},
